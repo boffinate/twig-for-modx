622de328096e840a4cfe38ecb4e04a0c163b7502
Peter Bowyer <peter@mapledesign.co.uk>
2026-03-15 18:10:16 +0000

    Missed from earlier commits

--- core/components/twig/elements/plugins/TwigContentBlocks.php
+++ core/components/twig/elements/plugins/TwigContentBlocks.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 /**
  * Listen to the event: ContentBlocks_BeforeParse
  *
+ * @var \MODX\Revolution\modX $modx
  * @var string $tpl
  * @var array<string, mixed> $phs
  */
 
 /** @var \Boffinate\Twig\Twig $twig */
-$twig = $this->modx->services->get('twigparser');
+$twig = $modx->services->get('twigparser');
+if (!is_array($phs)) {
+    $matches = [];
+    $modx->parser->collectElementTags($tpl, $matches);
+    if (!empty($matches)) {
+        // Strip the leading '+' from the placeholder name
+        $phs = [substr($matches[0][1], 1) => $phs];
+    } else {
+        $phs = ['value' => $phs];
+    }
+}
+
 return $twig->renderString($tpl, $phs);
